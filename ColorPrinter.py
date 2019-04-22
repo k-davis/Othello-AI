68af20d563ffcc055ddaadee0570f10b5b18566c
k-davis <kaseydavis216@gmail.com>
2019-04-22 13:01:33 -0700

    Added specs and background colors

--- ColorPrinter.py
+++ ColorPrinter.py
@@ -1,18 +1,33 @@
 
-BLACK = '\u001b[30m'
-RED = '\u001b[31m'
-GREEN = '\u001b[32m'
-YELLOW = '\u001b[33m'
-BLUE = '\u001b[34m'
-MAGENTA = '\u001b[35m'
-CYAN = '\u001b[36m'
-WHITE = '\u001b[37m'
+F_BLACK = '\u001b[30m'
+F_RED = '\u001b[31m'
+F_GREEN = '\u001b[32m'
+F_YELLOW = '\u001b[33m'
+F_BLUE = '\u001b[34m'
+F_MAGENTA = '\u001b[35m'
+F_CYAN = '\u001b[36m'
+F_WHITE = '\u001b[37m'
+
+BK_BLACK = '\u001b[40m'
+BK_RED = '\u001b[41m'
+BK_GREEN = '\u001b[42m'
+BK_YELLOW = '\u001b[43m'
+BK_BLUE = '\u001b[44m'
+BK_MAGENTA = '\u001b[45m'
+BK_CYAN = '\u001b[46m'
+BK_WHITE = '\u001b[47m'
+
+
 RESET = '\u001b[0m'
+
+
 
 
 def cprint(color, text):
 	print(color + text + RESET)
 
+def cprint(background_color, text_color, text):
+	print(background_color + text_color + text + RESET)
 
 
 	
