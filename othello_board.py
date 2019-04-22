017b041a5ee7002b2d16b951a382e7448ce678d3
k-davis <kaseydavis216@gmail.com>
2019-04-22 15:09:41 -0700

    Added make move function

--- othello_board.py
+++ othello_board.py
@@ -36,6 +36,49 @@ class OthelloBoard:
                     self.draw_b()
             print()
 
+    def make_a_move(self, token, row, col):
+        row = row - 1
+        col = ord(col) - ord('A')
+        self.board[row][col] = token
+
+        other_token = B if token == W else W
+
+        # up
+        self._make_move_traverse(token, other_token, row-1, col, -1, 0)
+        # up right
+        self._make_move_traverse(token, other_token, row-1, col+1, -1, 1)
+        # right
+        self._make_move_traverse(token, other_token, row, col+1, 0, 1)
+        # down right
+        self._make_move_traverse(token, other_token, row+1, col+1, 1, 1)
+        # down
+        self._make_move_traverse(token, other_token, row+1, col, 1, 0)
+        # down left
+        self._make_move_traverse(token, other_token, row+1, col-1, 1, -1)
+        # left
+        self._make_move_traverse(token, other_token, row, col-1, 0, -1)
+        # up left
+        self._make_move_traverse(token, other_token, row-1, col-1, -1, -1)
+
+        self.draw_board()
+        
+
+    def _make_move_traverse(self, my_tkn, other_tkn, cur_r, cur_c, move_r, move_c):
+        if self.board[cur_r][cur_c] == None:
+            return False
+
+        if self.board[cur_r][cur_c] == my_tkn:
+            return True
+        
+        # if valid move
+        if self._make_move_traverse(my_tkn, other_tkn, cur_r + move_r, cur_c + move_c, move_r, move_c):
+            # flip tokens while backtracking
+            self.board[cur_r][cur_c] = my_tkn
+            return True
+
+        return False
+
+
     def draw_w(self):
         cp.cprint(cp.BK_DK_RED, cp.F_WHITE, W + ' ')
 
