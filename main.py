e55add74ac9969ab4d87edc2449a72a9bf0e814f
k-davis <kaseydavis216@gmail.com>
2019-04-24 16:46:38 -0700

    Turn main into a class and added token selection

--- main.py
+++ main.py
@@ -13,26 +13,15 @@ Usage 1:  python3 <program name>
 import othello_board
 import ColorPrinter as cp
 
+class OthelloAI:
+    def __init__(self):
+        self.board = othello_board.OthelloBoard()
+        self.my_token = self.get_my_color()
+        
 
-def main():
-    new_board = othello_board.OthelloBoard()
-    new_board.draw_board()
-    get_move()
-
-
-def get_move():
-    move = input("Choose position (R C): ")
-    is_length_three = is_col_valid = is_row_valid = False
-
-    is_length_three = len(move) == 3
-
-    if is_length_three:
-        is_col_valid = ord(move[2]) in range(ord('A'), ord('I'))
-        is_row_valid = ord(move[0]) in range(ord('1'),ord('9'))
-
-    while not is_length_three or not is_col_valid or not is_row_valid:
-        print("Invalid selection. Must have the form <Row number> <Column char>.")
+    def get_move():
         move = input("Choose position (R C): ")
+        is_length_three = is_col_valid = is_row_valid = False
 
         is_length_three = len(move) == 3
 
@@ -40,10 +29,35 @@ def get_move():
             is_col_valid = ord(move[2]) in range(ord('A'), ord('I'))
             is_row_valid = ord(move[0]) in range(ord('1'),ord('9'))
 
-    return (move[0], move[2])
+        while not is_length_three or not is_col_valid or not is_row_valid:
+            print("Invalid selection. Must have the form <Row number> <Column char>.")
+            move = input("Choose position (R C): ")
 
-def format_move(pair):
-    return (int(pair[0]), pair[1])
+            is_length_three = len(move) == 3
+
+            if is_length_three:
+                is_col_valid = ord(move[2]) in range(ord('A'), ord('I'))
+                is_row_valid = ord(move[0]) in range(ord('1'),ord('9'))
+
+        return (move[0], move[2])
+
+    def format_move(pair):
+        return (int(pair[0]), pair[1])
+
+    def get_my_color(self):
+        color = input('Will you play as black (B) or white (W)? ')
+
+        while color not in ['B', 'W']:
+            print('Selection must be either \'B\' or \'W\'')
+            color = input('Will you play as black (B) or white (W)? ')
+        
+        return color
+
+    def display_score(self):
+        score = self.new_board.get_points()
+        print('  -- Score --')
+        print(' Black: ' + str(score[0]))
+        print(' White: ' + str(score[1]))
 
 if __name__ == "__main__":
-    main()
+    OthelloAI()
