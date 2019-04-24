5f97f8a9efd08dff10e7a43eb2e64d99172bd127
k-davis <kaseydavis216@gmail.com>
2019-04-24 16:10:49 -0700

    Added get user move function

--- main.py
+++ main.py
@@ -17,6 +17,30 @@ import ColorPrinter as cp
 def main():
     new_board = othello_board.OthelloBoard()
     new_board.draw_board()
+    get_move()
+
+
+def get_move():
+    move = input("Choose position (R C): ")
+    is_length_three = is_col_valid = is_row_valid = False
+
+    is_length_three = len(move) == 3
+
+    if is_length_three:
+        is_col_valid = ord(move[2]) in range(ord('A'), ord('I'))
+        is_row_valid = ord(move[0]) in range(ord('1'),ord('9'))
+
+    while not is_length_three or not is_col_valid or not is_row_valid:
+        print("Invalid selection. Must have the form <Row number> <Column char>.")
+        move = input("Choose position (R C): ")
+
+        is_length_three = len(move) == 3
+
+        if is_length_three:
+            is_col_valid = ord(move[2]) in range(ord('A'), ord('I'))
+            is_row_valid = ord(move[0]) in range(ord('1'),ord('9'))
+
+    return (move[0], move[2])
 
 
 if __name__ == "__main__":
